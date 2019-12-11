e5cf83d52ffb3fbeef5bb421aa9d0bd941e49011
Jens Svalgaard Kohrt <github@svalgaard.net>
2019-12-11 22:50:58 +0100

    Move 'magic' python lines to the top

--- 01-conditionals/startHere.py
+++ 01-conditionals/startHere.py
@@ -5,29 +5,33 @@
 #
 
 
+# MAGIC START
 # We'll later cover learn what happens here
 import math
 import random
-#
 
-# TODO: Write a print statement that displays the value and type of `pi`
 pi = math.pi
+r = 100 * random.random()  # a random float between 0 and 100
+i = random.randint(0, 1000)  # a random int between 0 and 1000 (inclusive)
+picked_fruit = random.choice(['grape', 'lemon', 'pear'])
+# MAGIC END
+
+# TODO: Write a print statement that displays the value and type of
+# the variable `pi`
 # print()???
 
 # TODO: Calculate + print the circumference of a circle with radius `r`
-r = 100 * random.random()  # a random float between 0 and 100
 # print()???
 
 # TODO: Write a conditional to print out if `i` is less than, equal to
 #       or greater than 500
-i = random.randint(0, 1000)  # a random int between 0 and 1000 (inclusive)
 # print()???
 
-# TODO: Write a conditional (if) that prints the color of the picked fruit
-picked_fruit = random.choice(['grape', 'lemon', 'pear'])
+# TODO: Write a conditional (if) that prints the color of the fruit
+# in the variable `picked_fruit`
 # print()???
 
-# TODO: Write a function that return the area of a circle of radius r
+# TODO: Write a function that return the area of a circle of radius `r`
 
 # TODO: Now call the function a few times to calculate the following answers
 
